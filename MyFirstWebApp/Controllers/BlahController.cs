1fae34a6e5b343501f4b1f77e9196edab6193195
Csbennett22 <90526059+Csbennett22@users.noreply.github.com>
2022-01-19 16:57:55 -0700

    Mission3

---
+++ MyFirstWebApp/Controllers/BlahController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApp.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GradeForm ()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult GradeForm (GradeFormModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFirstWebApp/Models/GradeFormModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApp.Models
+{
+    public class GradeFormModel
+    {
+        [Required]
+        [Range(0,100,
+            ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public sbyte Assignments { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public sbyte Gproject { get; set; }
+        [Required]
+        [Range(0, 100,
+            ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public sbyte Quiz { get; set; }
+        [Required]
+        [Range(0, 100,
+            ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public sbyte Exam { get; set; }
+        [Required]
+        [Range(0, 100,
+            ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public sbyte Intex { get; set; }
+    }
+}
